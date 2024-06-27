39fc5c696aa436cedaf911da4c7c0a73574ac6c1
jpjudd <jpj9649@rit.edu>
2024-06-27 01:57:16 -0500

    analyze added to do backtesting which computes statistics. This is canned and will need some modification. requirements file added to easily install packages with pip. smacross added as an output from running analyze as is

---
+++ analyze.py
@@ -0,0 +1,31 @@
+#source: https://kernc.github.io/backtesting.py/
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+from backtesting.test import SMA, GOOG
+
+class SmaCross(Strategy):
+    n1 = 10
+    n2 = 20
+
+    def init(self):
+        close = self.data.Close
+        self.sma1 = self.I(SMA, close, self.n1)
+        self.sma2 = self.I(SMA, close, self.n2)
+
+    def next(self):
+        if crossover(self.sma1, self.sma2):
+            self.buy()
+        elif crossover(self.sma2, self.sma1):
+            self.sell()
+
+
+bt = Backtest(
+                GOOG, 
+                SmaCross,               # This is a strategy does both indicator and buy/sell
+                cash=10000,             # Starting cash
+                commission=0.002,       # commission is 0.2% - may be low?
+                exclusive_orders=True,  
+            )
+
+output = bt.run()
+bt.plot()
